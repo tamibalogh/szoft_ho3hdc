e7afdc7fa153de678ae6eb395a147812b2e7a14c
tamibalogh <balogh.tamara.sara@gmail.com>
2024-03-14 13:08:20 +0100

    5. gyakorlat feladatok

---
+++ hajosKviz/VálaszGomb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hajosKviz
+{
+    internal class VálaszGomb : TextBox
+    {
+        public VálaszGomb()
+        {
+            ReadOnly = true;
+            BackColor = Color.Gray;
+            BorderStyle = BorderStyle.None;
+            Multiline = true;
+        }
+    }
+}
